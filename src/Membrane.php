00c45af880e8ca847dfffd16f80547c9f2bd5dda
Mārtiņš Tereško <martins.teresko@gmail.com>
2017-05-27 00:38:48 +0300

    Defining the initial structure of decorator

---
+++ src/Membrane.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ancile;
+
+use Ancile\Service\AccessControl;
+use Ancile\Exception\AccessDenied;
+use Ancile\Exception\MethodNotFound;
+
+class Membrane
+{
+
+    private $instance;
+    private $control;
+
+    public function __construct($instance, AccessControl $control)
+    {
+        $this->instance = $instance;
+        $this->control = $control;
+    }
+
+
+    public function __call($method, $arguments)
+    {
+        if (!$this->control->isAllowed(get_class($this->instance))) {
+            throw new AccessDenied;
+        }
+
+        if (!method_exists($this->instance, $method)) {
+            throw new MethodNotFound;
+        }
+
+        return call_user_func_array([$this->instance, $method], $arguments);
+    }
+}
